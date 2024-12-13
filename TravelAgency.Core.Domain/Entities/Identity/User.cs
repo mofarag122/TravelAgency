b77d9317228d4d9d55071272cff77f89b0088c20
Mohamed Ashraf <mohamedashraf35000@gmail.com>
2024-12-13 16:04:56 +0200

    Entities [ Common Entity , User - Address - Roles]

---
+++ TravelAgency.Core.Domain/Entities/Identity/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TravelAgency.Core.Domain.Entities._Common;
+
+namespace TravelAgency.Core.Domain.Entities.Identity
+{
+    public class User : Entity
+    {
+        public string FirstName { get; set; } = null!;
+
+        public string? LastName { get; set; }
+
+        public string UserName { get; set; } = null!;
+
+        public string Email { get; set; } = null!;
+
+        public string HashedPassword { get; set; } = null!;
+
+        public Address Address { get; set; } = null!;
+
+        public Roles Role { get; set; } 
+    }
+}
