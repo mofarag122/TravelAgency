38211cc2d25e3736dad3d876e3f8de4f83fbaf9d
Mohamed Ashraf <mohamedashraf35000@gmail.com>
2024-12-15 10:53:22 +0200

    Add Entities of The Notification Module

---
+++ TravelAgency.Core.Domain/Entities/Notification/Notification.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TravelAgency.Core.Domain.Entities._Common;
+
+namespace TravelAgency.Core.Domain.Entities.Notification
+{
+    public class Notification : Entity
+    {      
+        public int? UserId { get; set; }
+        public string? Recipient { get; set; } 
+        public string? Content { get; set; }
+       
+        public  TemplateName { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? SentAt { get; set; }
+        public bool IsSent { get; set; }
+        public Channels? Channel { get; set; } 
+        public string? FailureReason { get; set; } 
+    }
+}
