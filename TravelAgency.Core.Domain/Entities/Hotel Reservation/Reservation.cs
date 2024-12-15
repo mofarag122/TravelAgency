6e8b2f57373882b2413804e56073ccb984d4e67d
Mohamed Ashraf <mohamedashraf35000@gmail.com>
2024-12-15 22:22:12 +0200

    Add Entities of Hotel Reservation Module

---
+++ TravelAgency.Core.Domain/Entities/Hotel Reservation/Reservation.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using TravelAgency.Core.Domain.Entities._Common;
+
+namespace TravelAgency.Core.Domain.Entities.Hotel_Reservation
+{
+    public class Reservation : Entity
+    {
+        public int UserId { get; set; }
+
+        public int HotelId { get; set; }
+
+        public int RoomsId { get; set; }
+
+        public DateOnly StartDate { get; set; }
+
+        public DateOnly EndDate { get; set; }
+
+        public DateTime ReservationDate { get; set; } = DateTime.UtcNow;
+
+        public decimal Cost { get; set; }
+
+        public int Duration
+        {
+            get
+            {
+                return (EndDate.ToDateTime(new TimeOnly()) - StartDate.ToDateTime(new TimeOnly())).Days;
+            }
+        }
+
+    
+    }
+}
---
+++ TravelAgency.Core.Domain/Entities/Hotel Reservation/Room.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TravelAgency.Core.Domain.Entities._Common;
+
+namespace TravelAgency.Core.Domain.Entities.Hotel_Reservation
+{
+    public class Room : Entity
+    {
+        public RoomTypes RoomType { get; set; }
+
+        public List<string>? ImagePaths { get; set; }
+
+        public decimal PricePerNight { get; set; }
+    }
+}
